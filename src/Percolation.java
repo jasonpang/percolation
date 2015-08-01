cdf5b955ed7e70f0e07b76da08b026ddc5f6432f
Jason Pang <jasonpang2011@gmail.com>
2015-07-31 23:41:24 -0700

    WIP: Forgot

---
+++ src/Percolation.java
@@ -0,0 +1,59 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Percolation {
+    /**
+     * Creates a grid of `gridDimensions`, with all sites blocked.
+     *
+     * @param gridDimensions Specifies the width of height of the grid.
+     */
+    public Percolation(int gridDimensions)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Opens the site at (row i, column j).
+     * @param i The one-based row index.
+     * @param j The one-based column index.
+     */
+    public void open(int i, int j)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns whether the site at (row i, column j) is open.
+     * @param i The one-based row index.
+     * @param j The one-based column index.
+     * @return Returns true if the site is open; false otherwise.
+     */
+    public boolean isOpen(int i, int j)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns whether the site at (row i, column j) is full (opposite of open).
+     * @param i The one-based row index.
+     * @param j The one-based column index.
+     * @return Returns true if the site is open; false otherwise.
+     */
+    public boolean isFull(int i, int j)
+    {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public boolean percolates()
+    {
+
+    }// does the system percolate?
+
+    public static void main(String[] args)
+    {
+
+    }// test client (optional)
+}
